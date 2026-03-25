770d22a07b3e75bf9390f26c0580c37dc6213b98
paio22 <psazhin2005@yandex.ru>
2026-03-25 18:59:50 +0700

    Init

---
+++ Source/MyProject/Private/testScript.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "testScript.h"
+
+// Sets default values
+AtestScript::AtestScript()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AtestScript::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	
+}
+
+// Called every frame
+void AtestScript::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
